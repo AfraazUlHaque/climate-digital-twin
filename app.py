cb9cf100e45f8d6e98058456cf05cbb05fad6187
AfraazUlHaque <awesomeafraz3@gmail.com>
2026-06-24 12:53:30 +0530

    Test Streamlit deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Climate Digital Twin",
-    layout="wide"
-)
+st.set_page_config(page_title="Climate Digital Twin", layout="wide")
+
+st.title("AI-Powered Digital Twin of India's Climate")
+st.success("App deployed successfully 🚀")
+st.write("ISRO Hackathon PS-5 Dashboard")
